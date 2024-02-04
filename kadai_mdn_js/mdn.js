6d90740c3f05158aded99f33cde826eda7b480d6
osomatsusaaaan <154360132+osomatsusaaaan@users.noreply.github.com>
2024-02-05 01:55:32 +0900

    課題mdn_js　再提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const today = new Date(); 
+
+
+const year = today.getFullYear() + '年';
+const month = today.getMonth() + 1 + '月';
+const day = today.getDate() + '日';
+
+console.log(year + month + day);
