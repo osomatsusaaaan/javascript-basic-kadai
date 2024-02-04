40cb2aade87f9839fa97d8cdc09a4c5867c1d3ce
osomatsusaaaan <154360132+osomatsusaaaan@users.noreply.github.com>
2024-02-05 01:31:53 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,9 +0,0 @@
-const today = new Date(); 
-
-
-const year = (today.getFullYear()) + '年';
-const month = (today.getMonth()) + '月';
-const day = today.getDate() + '日';
-
-
-console.log(year + month + day);
